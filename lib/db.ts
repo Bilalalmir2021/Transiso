daa2874560718f4c43a0ba516a16f8cb35fe7b43
Bilalalmir2021 <bilalalmir1998@gmail.com>
2025-12-06 23:50:19 +0200

    modifier sur db

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "auth-db1485.hstgr.io",
+  user: "u317360254_dhia",
+  password: "99140777Parkour",
+  database: "u317360254_transiso",
 });
 
 export async function query<T>(sql: string, params?: any[]): Promise<T[]> {
